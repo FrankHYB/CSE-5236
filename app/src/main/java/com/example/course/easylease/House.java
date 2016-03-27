404735bcde728c2970351836aeabe6f597955e2d
Xiao Liang <izzyliang@gmail.com>
2016-03-27 17:16:58 -0400

    Add funciton to get latlng from address

--- app/src/main/java/com/example/course/easylease/House.java
+++ app/src/main/java/com/example/course/easylease/House.java
@@ -1,5 +1,16 @@
 package com.example.course.easylease;
 
+import android.content.Context;
+import android.location.Address;
+import android.location.Geocoder;
+
+import java.io.IOException;
+import java.util.List;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
 public class House {
     private double latitude;
     private double longitude;
@@ -55,5 +66,12 @@ public class House {
 
     public void setPrice(int price) {
         this.price = price;
+    }
+
+    private void getLatLng(Context context) throws IOException {
+        Geocoder geocoder = new Geocoder(context);
+        List<Address> addressList = geocoder.getFromLocationName(address, 1);
+        this.latitude = addressList.get(0).getLatitude();
+        this.longitude = addressList.get(0).getLongitude();
     }
 }
