49cf01671a77fc00030d407e3f8fb349813da021
Xiao Liang <izzyliang@gmail.com>
2016-03-27 20:15:23 -0400

    Remove unused imports

--- app/src/main/java/com/example/course/easylease/House.java
+++ app/src/main/java/com/example/course/easylease/House.java
@@ -7,10 +7,6 @@ import android.location.Geocoder;
 import java.io.IOException;
 import java.util.List;
 
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
-
 public class House {
     private double latitude;
     private double longitude;
