5965336b2e8a9abd9f057297fe3a4fef3c0842e4
FrankHYB <he.922@osu.edu>
2016-04-10 15:45:56 -0400

    fixed php file

--- app/src/OwnerInfo.php
+++ app/src/OwnerInfo.php
@@ -11,7 +11,7 @@ if(!empty($_POST['owner'])){
     $connection = new MongoClient('mongodb://jjshao:jj5914@ds045531.mlab.com:45531/ybhe');
     $db = $connection->ybhe;
     $users = $db->androidUser;
-    $query = $users->findOne(array('owner'=>$owner),array('email'=>true,'phoneNum'=>true));
+    $query = $users->findOne(array('username'=>$owner),array('email'=>true,'phoneNum'=>true));
     $response['email'] = $query['email'];
     $response['phoneNum'] = $query['phoneNum'];
     $connection->close();
